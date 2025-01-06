79d5253177c1cc23f986f713b60c78c0f4aca604
szxivk <50396748+szxivk@users.noreply.github.com>
2025-01-06 06:53:57 +0000

    Update files for javascript code-along
    - tried running javascript externally through javascript.js
    
    - added info on variable declaration and assignment

---
+++ odin-proj/webdev-foundations/code-along/variables-and-operators/javascript.js
@@ -0,0 +1,25 @@
+let name = "Sz";
+let message = "Hello!";
+
+console.log(name);
+console.log(message);
+
+let age = "24"; // initialization
+console.log(age);
+
+age = 25; // we only need to declare the variable once
+console.log(age);
+
+const pi = 3.14;
+console.log(pi);
+
+pi = 22;
+console.log(pi);
+
+/*
+-> two ways to declare a variable
+
+let: which we can re-assign.
+const: which we can’t re-assign and will throw an error if we try.
+
+*/
