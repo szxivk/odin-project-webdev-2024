a9b3f2e33e85e674b433f09ec8d87010deb863e4
szxivk <50396748+szxivk@users.noreply.github.com>
2025-02-05 08:24:57 +0000

    Add fizzbuzz problem solution to code-along folder
    - just the pseudocode for now

---
+++ odin-proj/webdev-foundations/code-along/fizzbuzz.js
@@ -0,0 +1,6 @@
+// Ask user for a Number
+// loop from 1 to that Number
+// for numbers divisible by 3 print fizz
+// for numbers divisible by 5 print buzz
+// for numbers that are divisible by both print fizzbuzz
+// end
