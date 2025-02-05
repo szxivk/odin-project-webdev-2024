1590bc7a7f89d5d0771d0132deaee7fdb3791f11
szxivk <50396748+szxivk@users.noreply.github.com>
2025-02-05 09:00:48 +0000

    Update solution for fizzbuzz.js problem
    - installed prompt-sync using npm

--- odin-proj/webdev-foundations/code-along/fizzbuzz.js
+++ odin-proj/webdev-foundations/code-along/fizzbuzz.js
@@ -1,6 +1,21 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
 // Ask user for a Number
+const num = Number(prompt("Enter Number: "));
 // loop from 1 to that Number
-// for numbers divisible by 3 print fizz
-// for numbers divisible by 5 print buzz
-// for numbers that are divisible by both print fizzbuzz
+for (let i = 1; i <= num; i++) {
+  // for numbers that are divisible by both print fizzbuzz
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+    // for numbers divisible by 3 print fizz
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+    // for numbers divisible by 5 print buzz
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
 // end
